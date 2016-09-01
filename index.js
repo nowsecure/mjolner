1704f302d31deae3502eca5e7a7bdced2e979648
Ole André Vadla Ravnås <oleavr@gmail.com>
2016-09-01 18:46:10 +0200

    Expose more of the inner APIs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const {toCYON} = require('./lib/cyon');
 const objc = require('./lib/objc');
 const types = require('./lib/types');
 
@@ -7,5 +8,17 @@ module.exports = {
   register() {
     types.register();
     objc.register();
+  },
+  add(name, details) {
+    return types.add(name, details);
+  },
+  lookup(name) {
+    return objc.lookup(name);
+  },
+  complete(prefix) {
+    return objc.complete(prefix);
+  },
+  toCYON(value) {
+    return toCYON(value);
   }
 };
